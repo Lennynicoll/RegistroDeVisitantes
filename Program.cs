1672d822fea1dbe3dd7ce032e652e2c95a0b6441
Lennynicoll <lennynunez55@gmail.com>
2026-07-18 18:01:06 -0400

    Agrego Swagger y limpieza de archivos duplicados

--- Program.cs
+++ Program.cs
@@ -15,7 +15,16 @@ builder.Services.AddScoped<IVisitaRepository, VisitaRepository>();
 builder.Services.AddControllers()
     .AddApplicationPart(typeof(RegistroVisitantes.Infrastructure.Controllers.VisitantesController).Assembly);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapControllers();
 
