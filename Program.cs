4a7fdf15b2b74fff39359621717dc56646f28539
Lennynicoll <lennynunez55@gmail.com>
2026-07-18 19:31:45 -0400

    Infrastructure: actualizar Context y Program.cs

--- Program.cs
+++ Program.cs
@@ -13,9 +13,43 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddScoped<IVisitanteRepository, VisitanteRepository>();
 builder.Services.AddScoped<IVisitaRepository, VisitaRepository>();
+builder.Services.AddScoped<IPacienteRepository, PacienteRepository>();
+builder.Services.AddScoped<IMedicoRepository, MedicoRepository>();
+builder.Services.AddScoped<IEspecialidadRepository, EspecialidadRepository>();
+builder.Services.AddScoped<IDepartamentoRepository, DepartamentoRepository>();
+builder.Services.AddScoped<ICitaRepository, CitaRepository>();
+builder.Services.AddScoped<IHistorialClinicoRepository, HistorialClinicoRepository>();
+builder.Services.AddScoped<IDiagnosticoRepository, DiagnosticoRepository>();
+builder.Services.AddScoped<IMedicamentoRepository, MedicamentoRepository>();
+builder.Services.AddScoped<IRecetaMedicaRepository, RecetaMedicaRepository>();
+builder.Services.AddScoped<IDetalleRecetaRepository, DetalleRecetaRepository>();
+builder.Services.AddScoped<ISeguroMedicoRepository, SeguroMedicoRepository>();
+builder.Services.AddScoped<IPacienteSeguroRepository, PacienteSeguroRepository>();
 
 builder.Services.AddScoped<IVisitanteService, VisitanteService>();
 builder.Services.AddScoped<IVisitaService, VisitaService>();
+builder.Services.AddScoped<IPacienteService, PacienteService>();
+builder.Services.AddScoped<IMedicoService, MedicoService>();
+builder.Services.AddScoped<IEspecialidadService, EspecialidadService>();
+builder.Services.AddScoped<IDepartamentoService, DepartamentoService>();
+builder.Services.AddScoped<ICitaService, CitaService>();
+builder.Services.AddScoped<IHistorialClinicoService, HistorialClinicoService>();
+builder.Services.AddScoped<IDiagnosticoService, DiagnosticoService>();
+builder.Services.AddScoped<IMedicamentoService, MedicamentoService>();
+builder.Services.AddScoped<IRecetaMedicaService, RecetaMedicaService>();
+builder.Services.AddScoped<IDetalleRecetaService, DetalleRecetaService>();
+builder.Services.AddScoped<ISeguroMedicoService, SeguroMedicoService>();
+builder.Services.AddScoped<IPacienteSeguroService, PacienteSeguroService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllers()
     .AddApplicationPart(typeof(RegistroVisitantes.Infrastructure.Controllers.VisitantesController).Assembly);
@@ -30,6 +64,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowReact");
 
 app.MapControllers();
 
--- RegistroVisitantes.Infrastructure/Context/ApplicationDbContext.cs
+++ RegistroVisitantes.Infrastructure/Context/ApplicationDbContext.cs
@@ -12,5 +12,17 @@ namespace RegistroVisitantes.Infrastructure.Context
 
         public DbSet<Visitante> Visitantes { get; set; }
         public DbSet<Visita> Visitas { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Medico> Medicos { get; set; }
+        public DbSet<Especialidad> Especialidades { get; set; }
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Cita> Citas { get; set; }
+        public DbSet<HistorialClinico> HistorialesClinicos { get; set; }
+        public DbSet<Diagnostico> Diagnosticos { get; set; }
+        public DbSet<Medicamento> Medicamentos { get; set; }
+        public DbSet<RecetaMedica> RecetasMedicas { get; set; }
+        public DbSet<DetalleReceta> DetallesReceta { get; set; }
+        public DbSet<SeguroMedico> SegurosMedicos { get; set; }
+        public DbSet<PacienteSeguro> PacienteSeguros { get; set; }
     }
 }
